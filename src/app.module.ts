75e173451ba28c2bd501c094f9b75f7f3f7dbb1a
Chaya <dajjbt3@gmail.com>
2021-02-13 15:44:20 +0100

    connected to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [PostsModule, AuthModule],
+  imports: [PostsModule, AuthModule, TypeOrmModule.forRoot(typeormConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,10 +3,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeormConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
-  port: 47383,
+  port: 47333,
   username: 'user',
   password: '1234',
-  database: 'tasks',
+  database: 'social-media-db',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
