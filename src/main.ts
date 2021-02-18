96c1b5a3e8e0a5d8dbfa61b2dc3492b6acb3cb18
Chaya <dajjbt3@gmail.com>
2021-02-18 09:21:21 +0100

    changes to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'https://plexxis.thefalcontech.com',
+      'https://practical-ardinghelli-46288e.netlify.app',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:8080',
+      'http://localhost:8081',
+    ],
+    methods: 'GET, POST, OPTIONS, PATCH, DELETE',
+  });
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
