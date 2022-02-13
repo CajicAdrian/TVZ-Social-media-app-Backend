487d22ca4490b9605af78dcefe1f873568a65560
Chaya <dajjbt3@gmail.com>
2022-02-13 11:29:53 +0100

    edit and delete posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
   UseGuards,
   UsePipes,
@@ -13,6 +15,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
 import { CreatePostDto } from './dto/create-post.dto';
+import { EditPostDto } from './dto/edit-post.dto';
 import type { Post as PostEntity } from './post.entity';
 import { PostsService } from './posts.service';
 
@@ -27,11 +30,8 @@ export class PostsController {
   }
 
   @Get('/:id')
-  getPostById(
-    @Param('id', ParseIntPipe) id: number,
-    @GetUser() user: User,
-  ): Promise<PostEntity> {
-    return this.postsService.getPostById(id, user);
+  getPostById(@Param('id', ParseIntPipe) id: number): Promise<PostEntity> {
+    return this.postsService.getPostById(id);
   }
 
   @Post()
@@ -41,5 +41,22 @@ export class PostsController {
     @GetUser() user: User,
   ): Promise<PostEntity> {
     return this.postsService.createPost(createPostDto, user);
+  }
+
+  @Patch('/:id')
+  updatePostById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() changes: EditPostDto,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.postsService.updatePostById(id, user, changes);
+  }
+
+  @Delete('/:id')
+  deletePostById(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.postsService.deletePostById(id, user);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,8 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/auth/user.entity';
 import { ImageRepository } from 'src/images/image.repository';
 import { CreatePostDto } from './dto/create-post.dto';
+import { EditPostDto } from './dto/edit-post.dto';
 import { Post } from './post.entity';
 import { PostRepository } from './post.repository';
 
@@ -18,9 +23,9 @@ export class PostsService {
     return this.postRepository.getPosts();
   }
 
-  async getPostById(id: number, user: User): Promise<Post> {
-    const found = await this.postRepository.findOne({
-      where: { id, userId: user.id },
+  async getPostById(id: number): Promise<Post> {
+    const found = await this.postRepository.findOne(id, {
+      relations: ['user', 'images'],
     });
 
     if (!found) {
@@ -32,5 +37,32 @@ export class PostsService {
   async createPost(createPostDto: CreatePostDto, user: User): Promise<Post> {
     const images = [await this.imageRepository.findOne(createPostDto.imageId)];
     return this.postRepository.createPost(createPostDto, user, images);
+  }
+
+  async updatePostById(
+    id: number,
+    user: User,
+    changes: EditPostDto,
+  ): Promise<void> {
+    const post = await this.getPostById(id);
+
+    if (post.user.id !== user.id) {
+      throw new ForbiddenException("Cannot edit other user's posts");
+    }
+
+    await this.postRepository.update(id, changes);
+  }
+
+  async deletePostById(id: number, user: User): Promise<void> {
+    const post = await this.getPostById(id);
+
+    if (post.user.id !== user.id) {
+      throw new ForbiddenException("Cannot delete other user's posts");
+    }
+
+    await Promise.all(
+      post.images.map((img) => this.imageRepository.delete(img.imageId)),
+    );
+    await this.postRepository.delete(id);
   }
 }
